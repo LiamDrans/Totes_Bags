7e5be4c27f36176573428502de133ee5fd8e7059
russgooday <rpg_digital@yahoo.co.uk>
2024-08-13 10:46:56 +0100

    add docstrings to functions

--- db/crud_functions.py
+++ db/crud_functions.py
@@ -1,14 +1,25 @@
+''' initial crud operations for the database '''
+
 import json
-from pprint import pprint
+from typing import Dict
 from connection import CreateConnection
 from pg8000.native import identifier
 from utils.helpers import format_response
 from utils.converters import convert_datetime, convert_decimal
 
-def fetch_table(table_name: str) -> dict:
-    ''' fetches table by table name formatting the output '''
+def fetch_table(table_name: str, conn: CreateConnection=None) -> Dict:
+    """ fetches table data and formats it into JSON formattable data
+
+    Arguments:
+        table_name (str): name of the table to query
+        conn: the database connection
+
+    Returns:
+        dict: table data formatted into a dictionary
+    """
     with CreateConnection() as conn:
-        rows = conn.run(f'SELECT * FROM {identifier(table_name)} LIMIT 5')
+        rows = conn.run(f'SELECT * FROM {identifier(table_name)};')
+
         if conn.row_count:
             return format_response(
                 conn.columns,
@@ -20,5 +31,4 @@ def fetch_table(table_name: str) -> dict:
 
 if __name__ == '__main__':
     sales_orders = fetch_table('sales_order')
-    # pprint(sales_orders, indent=2)
     print(json.dumps(sales_orders, indent=2))
