c934f1fc8104e6b0587d7eb0b4b46959c21c7f93
PhoenixBreaker <karlsmitchy3765@gmail.com>
2024-08-13 10:20:46 +0100

    Adds s3 terraform script

---
+++ terraform/s3.tf
@@ -0,0 +1,51 @@
+terraform {
+    required_providers {
+        aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "data_ingestion" {
+    bucket_prefix = "totes-data-"
+
+    tags = {
+        Name ="DataIngestionBucket"
+        Environment = "Extract"
+    }
+}
+
+resource "aws_s3_bucket" "code_bucket" {
+    bucket_prefix = "totes-lambda-code-"
+
+    tags = {
+        Name = "LambdaCodeBucket"
+        Environment = "Extract"
+    }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "data_ingestion_lifecycle" {
+    bucket = aws_s3_bucket.data_ingestion.id
+
+    rule {
+        id ="TransitionToInfrequentAccess"
+    filter {
+        prefix = ""
+    }
+    transition {
+        days = 30
+        storage_class = "STANDARD_IA"
+    }
+
+    expiration {
+        days = 90
+    }
+
+    status = "Enabled"
+    }
+}
