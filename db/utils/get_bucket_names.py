a570217358b4f8898d98779552df331fd81539c3
LazySusanoo <liam.drans@gmail.com>
2024-08-14 14:46:13 +0100

    updated lambda handler with function to pull bucket name

---
+++ db/utils/get_bucket_names.py
@@ -0,0 +1,22 @@
+"""these functions will get the bucket list and return the bucket"""
+import boto3
+
+
+def get_data_bucket_name():
+    """getting the data bucket name"""
+    s3 = boto3.client('s3')
+    
+    response = s3.list_buckets()
+    bucket_prefix = "totes-data-"
+    
+    try:
+        if not response['Buckets']:
+            raise ValueError("No buckets found in S3")
+    
+        for bucket in response['Buckets']:
+            if bucket['Name'].startswith(bucket_prefix):
+                return bucket['Name']
+        raise ValueError(f"No bucket found with prefix: {bucket_prefix}")
+    except ValueError as e:
+        print(f"An error occurred while accessing S3: {e}")
+        raise
