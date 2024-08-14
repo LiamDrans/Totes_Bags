2ec57e6eab6f0cee26ac7746de5a538be5f09171
Zameer Mohamed <zameermohamed@hotmail.com>
2024-08-14 16:44:14 +0100

    lambda policies update to include secrets manager

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -31,14 +31,49 @@ data "aws_iam_policy_document" "s3_document" {
   }
 }
 
+data "aws_iam_policy_document" "secrets_manager_document" {
+    statement {
+
+    actions = ["secretsmanager:GetSecretValue",
+				"secretsmanager:DescribeSecret"]
+
+    resources = [
+        "arn:aws:secretsmanager:eu-west-2:730335560557:secret:totesys_db-*",
+    ]
+  }
+}
+
+
+# data "aws_iam_policy_document" "rds_document" {
+#     statement {
+
+#     actions = ["secretsmanager:GetSecretValue",
+# 				"secretsmanager:DescribeSecret"]
+
+#     resources = [
+#         "arn:aws:secretsmanager:eu-west-2:730335560557:secret:totesys_db-*",
+#     ]
+#   }
+# }
+
 resource "aws_iam_policy" "s3_policy" {
     name_prefix = "s3-policy-totes-lambda-"
     policy      = data.aws_iam_policy_document.s3_document.json
 }
 
+resource "aws_iam_policy" "secrets_manager_policy" {
+    name_prefix = "secrets-manager-policy-totes-lambda-"
+    policy      = data.aws_iam_policy_document.secrets_manager_document.json
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
     role       = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_secrets_manager_policy_attachment" {
+    role       = aws_iam_role.lambda_role.name
+    policy_arn = aws_iam_policy.secrets_manager_policy.arn
 }
 
 
