484eb3995aa41df4e7d976460cd80093de466713
PhoenixBreaker <karlsmitchy3765@gmail.com>
2024-08-15 10:57:19 +0100

    Adds eventbridge terraform that when constructed will fire every 30 mins

---
+++ terraform/eventbridge.tf
@@ -0,0 +1,20 @@
+resource "aws_cloudwatch_event_rule" "every_30_minutes" {
+  name        = "trigger_lambda_every_30_minutes"
+  description = "Triggers Lambda every 30 minutes"
+  schedule_expression = "rate(30 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.every_30_minutes.name
+  target_id = "extract"
+  arn       = aws_lambda_function.extract.arn
+}
+
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.extract.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_30_minutes.arn
+}
