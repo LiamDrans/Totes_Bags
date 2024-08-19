da849fc7c0d6f0e2ff0021730bc1cb1c096c323e
Zameer Mohamed <zameermohamed@hotmail.com>
2024-08-19 16:55:09 +0100

    including liams code as off 19/08/24 12:00

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,23 +1,35 @@
-"""inital terraform checks"""
-from pg8000.native import Connection
+"""Script for lambda_handler"""
+
+import datetime
+import boto3
+from crud_functions import fetch_all_tables
+from utils.get_bucket_names import get_data_bucket_name
+from utils.json_io import save_json
 
 def lambda_handler(event, context):
-    """Function to form connection to the totesys database"""
 
-    connection = Connection(
-                    "project_team_5",
-                    host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
-                    password="EE7T5U5pMrH3InX",
-                    database="totesys",
-                    port="5432",
-                )
-    sql=event
-    sql=context
-    sql = """
-            SELECT
-                table_name
-            FROM
-                information_schema.tables
-            WHERE table_schema='public' AND table_name ~ '^[a-z]'
-        """
-    return connection.run(sql)
+    """lambda function to put zip on S3 Bucket"""
+
+    full_data = fetch_all_tables()
+    full_data_json = save_json(full_data)
+    bucket_name = get_data_bucket_name()
+
+    s3 = boto3.client('s3', region_name='eu-west-2')
+    
+    s3.put_object(
+        Body=full_data_json,
+        Bucket=bucket_name,
+        Key=f'{datetime.datetime.now().year}/{datetime.datetime.strptime(f"{datetime.datetime.now().month}", "%m").strftime("%B")}/{datetime.datetime.now().day}/{datetime.datetime.now().strftime('%H:%M:%S')}_db_totes.json'
+    )
+
+    s3.put_object(
+        Body=full_data_json,
+        Bucket=bucket_name,
+        Key='latest_db_totes.json'
+    )
+
+if __name__ == '__main__':
+    lambda_handler(1,2)
+    print ('pass')
+    # with open('./json_files/zzz_db_totesys.json', 'w', encoding='utf-8') as f:
+    #     f.write(save_json(fetch_all_tables()))
---
+++ src/extract/utils/json_io.py
@@ -0,0 +1,26 @@
+''' Converter functions '''
+import json
+from typing import Dict
+from decimal import Decimal
+from datetime import datetime
+
+# Custom class extends json.JSONEncoder
+class CustomJSONEncoder(json.JSONEncoder):
+    ''' Custom JSON Encoder '''
+    def default(self, o):
+
+        if isinstance(o, datetime):
+            return o.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+        if isinstance(o, Decimal):
+            return float(o)
+
+        # Default behavior for all other types
+        return super().default(o)
+
+def save_json(data: Dict) -> None:
+    ''' saves json file to destination '''
+    try:
+        return json.dumps(data, separators=(',', ':'), cls=CustomJSONEncoder)
+    except ValueError as e:
+        print(f"An error occurred while running save_json: {e}")
