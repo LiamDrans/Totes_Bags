79f2369f455ac209b43c5e9ac97ee74dc3d110fc
russgooday <rpg_digital@yahoo.co.uk>
2024-08-23 01:06:52 +0100

    Refactor: - combine fetch_tables and fetch_updated_rows into one function - change conn, last_time_queried to keyword args - return latest_query_time and tables

--- src/extract/app/db/db_crud_functions.py
+++ src/extract/app/db/db_crud_functions.py
@@ -1,44 +1,44 @@
 """ initial crud operations for the database """
 
-from typing import Optional, Union, Dict, List
-from pg8000.native import Connection, identifier
+from datetime import datetime, timedelta
+from typing import Optional, Union, Dict, List, Tuple
+from pg8000.native import Connection, Error, identifier, TIMESTAMP
 from .connection import CreateConnection
 from ..utils.helpers import format_response
 
 
-def query_db(sql: str, conn: Optional[Connection] = None) -> Union[List, None]:
+def query_db(
+    sql: str, /,
+    placeholders: Optional[Dict] = None,
+    conn: Optional[Connection] = None,
+    types: Optional[Dict] = None
+) -> Union[List, None]:
     """Query the database and return the result as a list, or None if no rows are returned.
     Args:
-        sql (str): query string
-        conn (Connection): the database connection
+        sql (str): query string,
+        placeholders (Dict): optional query placeholder parameters,
+        conn (Connection): optional database connection
+        types (Dict): optional dictionary of types to cast the results to
 
     Returns:
         List: a list of rows from the database or None if no rows are returned
     """
-    if not isinstance(conn, Connection):
-        with CreateConnection() as new_conn:
-            return new_conn.run(sql)
-    return conn.run(sql)
+    if isinstance(conn, Connection):
+        return conn.run(sql, **(placeholders or {}), types=types)
+
+    with CreateConnection() as new_conn:
+        return new_conn.run(sql, **(placeholders or {}), types=types)
 
 
-def fetch_table(table_name: str, conn: Optional[Connection] = None) -> Union[Dict, bool]:
+def fetch_table_rows(
+    table_name: str, /,
+    last_time_queried: Optional[str] = None,
+    conn: Optional[Connection] = None
+) -> Union[Dict, bool]:
     """fetches table data and formats it
     Args:
         table_name (str): name of the table to query
-        conn (Connection): the database connection
-
-    Returns:
-        Dict: table data formatted into a dictionary or False if no data is returned
-    """
-    if rows := query_db(f"SELECT * FROM {identifier(table_name)};", conn):
-        return format_response(conn.columns, rows, label=table_name)
-    return False
-
-
-def fetch_updated_rows(table_name: str, conn: Optional[Connection] = None) -> Union[Dict, bool]:
-    """fetches rows from the table that have been updated in the last 30 minutes
-    Args:
-        table_name (str): name of the table to query
+        last_time_queried (str): optional time to query from
         conn (Connection): the database connection
 
     Returns:
@@ -46,12 +46,18 @@ def fetch_updated_rows(table_name: str, conn: Optional[Connection] = None) -> Un
     """
 
     sql = f"""
-        SELECT * FROM {identifier(table_name)}
-        WHERE last_updated >= NOW() - interval '30 minutes';
+        SELECT *
+        FROM {identifier(table_name)}
+        WHERE :last_time_queried is NULL OR last_updated >= :last_time_queried;
     """
 
-    if rows:= query_db(sql, conn):
+    placeholders = {'last_time_queried': last_time_queried}
+    # cast the last_time_queried to a timestamp, necessary for NULL comparison
+    types = {'last_time_queried': TIMESTAMP}
+
+    if rows := query_db(sql, placeholders=placeholders, conn=conn, types=types):
         return format_response(conn.columns, rows, label=table_name)
+
     return False
 
 
@@ -66,20 +72,35 @@ def fetch_table_names(conn: Optional[Connection] = None) -> Union[List, bool]:
         WHERE table_schema='public' AND table_name ~ '^[a-z]'
     """
 
-    if rows:= query_db(sql, conn):
+    if rows:= query_db(sql, conn=conn):
         return [row[0] for row in rows]
     return False
 
 
-def fetch_all_tables(updates = False) -> Union[List, bool]:
-    """fetches data from all the tables"""
+def fetch_all_tables(last_time_queried: Optional[str] = None) -> Tuple[str, List]:
+    """fetches data from all the tables
+    Args:
+        last_time_queried (str): optional time to query from
+    Returns:
+        Dict: Dictionary with the last time queried and a list of tables with their data
+    """
     with CreateConnection() as conn:
         try:
-            fetcher=fetch_updated_rows if updates else fetch_table
-            return [row for name in fetch_table_names(conn) if (row:= fetcher(name, conn))]
-        except TypeError as e:
-            print(str(e))
+            return (
+                datetime.now().isoformat(),
+                [
+                    row for name in fetch_table_names(conn)
+                    if (row:= fetch_table_rows(
+                        name,
+                        last_time_queried=last_time_queried,
+                        conn=conn
+                    ))
+                ]
+            )
+        except Error as err:
+            print(str(err))
             raise
 
+
 if __name__ == "__main__":
-    print(fetch_all_tables())
+    print(fetch_all_tables((datetime.now() - timedelta(hours=6)).isoformat()))
